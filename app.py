f482d304ea72d86f5429bf0864b78bad5c225708
George Zakharov <george_zakharov@mail.ru>
2020-01-06 02:20:15 +0300

    Update app

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ app = flask.Flask(__name__, template_folder='templates')
 
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def main():
     if flask.request.method == 'GET':
         return render_template('main.html')
 
     if flask.request.method == 'POST':
+        return render_template('main.html')
         temp = 1
         with open('model.pickle', 'rb') as fh:
             loaded_model = pickle.load(fh)
