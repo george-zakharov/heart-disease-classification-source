6781e737148e7d7ee57754829b8a74b19594b9f2
George Zakharov <george_zakharov@mail.ru>
2020-01-06 02:09:58 +0300

    Update app

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import flask
 from flask import render_template
 import pickle
-import sklearn
 import random
 import pandas as pd
 
@@ -9,7 +8,6 @@ app = flask.Flask(__name__, template_folder='templates')
 
 
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
 def main():
     if flask.request.method == 'GET':
         return render_template('main.html')
