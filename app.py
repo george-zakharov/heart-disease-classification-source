7b81d9b0b6c116d6eacef7a4889e6866f681607e
George Zakharov <george_zakharov@mail.ru>
2020-01-06 18:23:24 +0300

    Update app

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import flask
 from flask import render_template
 import pickle
-import pandas as pd
 
 app = flask.Flask(__name__, template_folder='templates')
 
@@ -9,7 +8,7 @@ app = flask.Flask(__name__, template_folder='templates')
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def main():
-    temp = 'no data'
+    temp = None
     if flask.request.method == 'GET':
         return render_template('main.html', result=temp)
 
@@ -19,10 +18,17 @@ def main():
 
         if 'Send' in flask.request.form:
             dic = flask.request.form.to_dict(flat=False)
-            print(dic)
-            del dic['Send']
-            df = pd.DataFrame(dic)
-            temp = loaded_model.predict(df)
+            if 'thal' not in dic:
+                # And yes, we need this line to fix some input issues, cuz this key can randomly be empty
+                dic['thal'] = '0'
+
+            temp = loaded_model.predict([[
+                float(dic['thalach'][0]), float(dic['oldpeak'][0]), float(dic['age'][0]), float(dic['chol'][0]),
+                float(dic['trestbps'][0]), int(dic['thal'][0]), int(dic['cp'][0]), int(dic['ca'][0]),
+                int(dic['exang'][0]), int(dic['slope'][0])
+            ]])
+            if temp is not None:
+                temp = str(temp[0])
 
         return render_template('main.html', result=temp)
 
